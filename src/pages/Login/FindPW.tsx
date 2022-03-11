cfd17146e652b185611145d84217e0b9771f573f
Ji Hyuk Ma <t90laser93@gmail.com>
2022-03-11 12:37:30 +0900

    오류수정

--- src/pages/Login/FindPW.tsx
+++ src/pages/Login/FindPW.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-// import MainButton from "../../components/Login/loginCommon/MainButton";
 import MainButton from "../../components/login/loginCommon/MainButton";
 import UnicornIcon from "../../components/login/loginCommon/UnicornIcon";
 import UserInputContainer from "../../components/login/loginCommon/UserInputContainer";
